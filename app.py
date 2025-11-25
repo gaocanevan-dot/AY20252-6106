0fef0ebe02362248cb1ac8023d9fa203b2d5ef1f
Evan <gaocanevan@gmail.com>
2025-11-25 10:12:11 +0000

    update paynow.htm;

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/result',methods=['GET','POST'])
-def main(): 
-    q = request.form.get("q")
-    print(q)
-    return render_template('main.html',query=q)
+@app.route('/paynow',methods=['GET','POST'])
+def paynow(): 
+
+    return render_template("paynow.html;")
 
 if __name__ == '__main__':
     app.run()
